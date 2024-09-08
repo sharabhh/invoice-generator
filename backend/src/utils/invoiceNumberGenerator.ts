cb8822b2c179c0334a076638e60888dbf402b3ac
sharabh mishra <sharabhmishra15@gmail.com>
2024-09-08 11:24:49 +0530

    new invoice route done not calculating total amounts yet

--- backend/src/utils/invoiceNumberGenerator.ts
+++ backend/src/utils/invoiceNumberGenerator.ts
@@ -1,12 +1,9 @@
 // generate a random 6 digit invoice number
 
-export default function invoiceNumberGenerator(){
-    var finalNumber
-    var multiplyFactor = 100000
-    for(var i=0; i<6;i++){
-        var number = (Math.random() * 9) + 1
-        finalNumber = Math.floor(number * multiplyFactor)
-        multiplyFactor/10
-    }
-    return finalNumber
+export default function invoiceNumberGenerator(): string {
+    // Generate a random 6-digit number
+    const min = 100000; // Minimum 6-digit number
+    const max = 999999; // Maximum 6-digit number
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    return randomNumber.toString();
 }
